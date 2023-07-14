4b72609de50de776f8dc4b15437e4ce3c9cc3fd3
Sarah <sarahkulsoom18@gmail.com>
2023-07-14 11:40:13 +0530

    Valid Palindrome

---
+++ TwoPointers/ValidPalindrome.java
@@ -0,0 +1,24 @@
+package TwoPointers;
+
+class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(c);
+            }
+        }
+
+        String str = sb.toString().toUpperCase();
+
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
