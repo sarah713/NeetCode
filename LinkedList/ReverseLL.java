b3b5c58d5e855f8a40ac29bf6ccba05496cde098
Sarah <sarahkulsoom18@gmail.com>
2023-07-18 12:51:17 +0530

    Reverse LinkedList

---
+++ LinkedList/ReverseLL.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+public class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
