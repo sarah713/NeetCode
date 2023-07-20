46bcb383f85b302298c5e1007d9382b6ce67c823
Sarah <sarahkulsoom18@gmail.com>
2023-07-20 11:02:05 +0530

    Merge K Sorted LinkedLists

---
+++ LinkedList/MergeKSortedLL.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+import java.util.PriorityQueue;
+
+public class MergeKSortedLL {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode node : lists) {
+            if (node != null) {
+                heap.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tmp = dummy;
+        while (!heap.isEmpty()) {
+            ListNode cur = heap.poll();
+            tmp.next = cur;
+            tmp = tmp.next;
+
+            if (cur.next != null) {
+                heap.offer(cur.next);
+            }
+
+        }
+        return dummy.next;
+    }
+}
