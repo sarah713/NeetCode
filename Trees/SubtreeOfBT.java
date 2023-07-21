bdd3c72be94043a147f1c141214213836d35f575
Sarah <sarahkulsoom18@gmail.com>
2023-07-21 15:47:19 +0530

    Subtree of a tree

---
+++ Trees/SubtreeOfBT.java
@@ -0,0 +1,30 @@
+package Trees;
+
+public class SubtreeOfBT {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == subRoot.val) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isIdentical(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        } else if (root == null || subRoot == null || root.val != subRoot.val) {
+            return false;
+        }
+        if (!isIdentical(root.left, subRoot.left)) {
+            return false;
+        }
+        if (!isIdentical(root.right, subRoot.right)) {
+            return false;
+        }
+        return true;
+    }
+}
