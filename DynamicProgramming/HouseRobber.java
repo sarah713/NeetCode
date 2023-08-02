c1c073980ab9d408bd9bba8e734dc790d07db6ef
Sarah <sarahkulsoom18@gmail.com>
2023-08-02 18:10:38 +0530

    House Robber

---
+++ DynamicProgramming/HouseRobber.java
@@ -0,0 +1,23 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class HouseRobber {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        Arrays.fill(dp, -1);
+        for (int i = 1; i < nums.length; i++) {
+            int take = nums[i];
+
+            if (i > 1) {
+                take += dp[i - 2];
+            }
+
+            int dont = dp[i - 1];
+            dp[i] = Math.max(take, dont);
+        }
+        return dp[nums.length - 1];
+    }
+}
