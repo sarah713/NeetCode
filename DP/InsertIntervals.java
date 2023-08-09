f336b0a3896c7d66468553a58ce1a031b66d4df4
Sarah <sarahkulsoom18@gmail.com>
2023-08-09 13:20:17 +0530

    Insert Interval

---
+++ DP/InsertIntervals.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertIntervals {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (newInterval[1] < interval[0]) {
+                res.add(newInterval);
+                newInterval = interval;
+            } else if (interval[1] < newInterval[0]) {
+                res.add(interval);
+            } else {
+                newInterval[0] = Math.min(newInterval[0], interval[0]);
+                newInterval[1] = Math.min(newInterval[1], interval[1]);
+            }
+        }
+        res.add(newInterval);
+
+        return res.toArray(new int[0][]);
+    }
+}
